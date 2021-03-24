e25fdc702bbe92efefc8825006cc4a89f131f538
Anderson Ramos <anderson.ramlug@gmail.com>
2021-03-24 13:18:45 -0300

    Update Simple.JSON.DataSet.pas

--- src/Simple.JSON.DataSet.pas
+++ src/Simple.JSON.DataSet.pas
@@ -5,6 +5,7 @@ interface
 uses
   VCl.StdCtrls,
 
+  System.Classes,
   System.SysUtils,
   System.JSON,
 
@@ -19,6 +20,7 @@ type
     procedure JSONArrayToDataSet(AValue: String);
   public
     procedure JSONToDataSet(AValue: String);
+    function DataSetToJSON: String;
   end;
 
 implementation
@@ -70,6 +72,84 @@ begin
     JSONArrayToDataSet(AValue)
   else
     JSONObjectToDataSet(AValue);
+end;
+
+function TSimpleJSONDataSetHelper.DataSetToJSON: String;
+var
+  FColumnName: String;
+  FJSONObject: TJsonObject;
+  FJSONArray: TJSONArray;
+  FPosition: TBookmark;
+begin
+  FJSONArray := TJSONArray.Create;
+  try
+    if Self.Active then
+    begin
+      Self.GotoBookmark(FPosition);
+      try
+        Self.DisableControls;
+        Self.First;
+
+        while not(Self.Eof) do
+        begin
+          FJSONObject := TJSONObject.Create;
+          try
+            for var I := 0 to Self.FieldDefs.Count-1 do
+            begin
+              FColumnName := Self.FieldDefs[I].Name;
+
+              case Self.Fields[I].Datatype of
+                ftBoolean:
+                begin
+                  if Self.Fields[I].AsBoolean = True then
+                    FJSONObject.AddPair(TJSONPair.Create(TJSONString.Create( FColumnName ), TJSONTrue.Create))
+                  else
+                    FJSONObject.AddPair(TJSONPair.Create(TJSONString.Create( FColumnName ), TJSONFalse.Create));
+                end;
+
+                ftInteger,
+                ftFloat,
+                ftSmallint,
+                ftWord,
+                ftCurrency :
+                begin
+                  FJSONObject.AddPair(TJSONPair.Create(TJSONString.Create( FColumnName ), TJSONNumber.Create(Self.Fields[I].Value)));
+                end;
+
+                ftDate:
+                begin
+                  FJSONObject.AddPair(TJSONPair.Create(TJSONString.Create( FColumnName ), TJSONString.Create(FormatDateTime('yyyy-mm-dd', Self.Fields[I].AsDateTime))));
+                end;
+
+                ftDatetime:
+                begin
+                  FJSONObject.AddPair(TJSONPair.Create(TJSONString.Create( FColumnName ), TJSONString.Create(FormatDateTime('yyyy-mm-dd hh:mm:ss', Self.Fields[I].AsDateTime))));
+                end;
+
+                ftTime:
+                begin
+                  FJSONObject.AddPair(TJSONPair.Create(TJSONString.Create( FColumnName ), TJSONString.Create(FormatDateTime('hh:mm:ss', Self.Fields[I].AsDateTime))));
+                end;
+
+              else
+                FJSONObject.AddPair(TJSONPair.Create(TJSONString.Create( FColumnName ),TJSONString.Create(Self.Fields[I].AsString)));
+              end;
+            end;
+          finally
+            FJSONArray.Add(FJSONObject);
+          end;
+          Self.Next;
+        end;
+      finally
+        Self.GotoBookmark(FPosition);
+        Self.FreeBookmark(FPosition);
+        Self.EnableControls;
+      end;
+    end;
+  finally
+    Result := FJSONArray.ToString;
+    FJSONArray.Free;
+  end;
 end;
 
 end.
